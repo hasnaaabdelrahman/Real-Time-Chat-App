ed4f679f557e745d62c67dc2e4d8ae788a8b1057
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-25 14:37:55 +0300

    feat: redirect to chat room

--- src/main/resources/static/js/main.js
+++
@@ -1,7 +0,0 @@
-joinForm = document.querySelector('#join-form');
-username = document.querySelector('#username');
-joinButton = document.querySelector('#join-button');
-chatBox = document.querySelector('#chat');
-chatForm = document.querySelector('#chat-form');
-message = document.querySelector('#message');
-sendButton = document.querySelector('#send-button');
