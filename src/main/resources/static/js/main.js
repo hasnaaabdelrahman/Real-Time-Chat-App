753a90b8e32adb6476c9119854bda62e29ddcada
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-25 12:02:08 +0300

    feat: add IDs to paages

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,8 @@
+joinForm = document.querySelector('#join-form');
+username = document.querySelector('#username');
+joinButton = document.querySelector('#join-button');
+chatBox = document.querySelector('#chat');
+chatForm = document.querySelector('#chat-form');
+message = document.querySelector('#message');
+senButton = document.querySelector('#send-button');
+
